622df4e8574837f4bc72a57e44320370cba9a322
Anshit-Gupta <anshitgupta777@gmail.com>
2026-09-22 22:56:24 +0530

    reading and sending json

--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import(
 	"fmt"
 	"net/http"
+	"encoding/json" 
 )
+
+
+//json respones : we use struct , then encode it using encoding/json 
+//***for JSON ,, struct key should always start from capital letter or else , it will be 
+//invisible to the json
+type  greetings struct{
+   Message string
+}
+
+type Longurl struct{
+	URL string 
+}
 
 //exatc fucntion handle to be made 
 func printmsg(w http.ResponseWriter , r *http.Request){
@@ -12,9 +25,21 @@ func printmsg(w http.ResponseWriter , r *http.Request){
 	//can also write this using w.Write([]byte(""))
 }
 
+func readjson(w http.ResponseWriter , r *http.Request){
+	var req Longurl 
+	err:= json.NewDecoder(r.Body).Decode(&req)//similar to sacnl(&s) , we give address of the varibale where we want it to be stored 
+	if err!=nil{
+		fmt.Fprintf(w,"error decoding : %v",err)
+		return 
+	}
+	fmt.Fprintf(w,"got:%s" , req.URL) 
+
+}
+
 func prams(w http.ResponseWriter , r * http.Request){
     name:=r.URL.Query().Get("name")
-	fmt.Fprintf(w,"hello %v ",name)
+	msg := greetings{Message: "hello "+name}  
+	json.NewEncoder(w).Encode(msg)
 }
 
 
@@ -22,6 +47,7 @@ func main(){
      
 	http.HandleFunc("/",printmsg) //does not return aything 
 	http.HandleFunc("/greet",prams)
+	http.HandleFunc("/shorten",readjson)
 	fmt.Println("server started at localhost 8080 ")
     err := http.ListenAndServe(":8080",nil) //this returns just a error and not usual res,err
 	if(err!=nil){
