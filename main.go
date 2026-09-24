2b96eb34809f941d1cd5864b9c8c5d11acf7ea65
Anshit-Gupta <anshitgupta777@gmail.com>
2026-09-24 23:19:40 +0530

    create shortcode+ handlers

--- main.go
+++ main.go
@@ -3,51 +3,82 @@ package main
 import(
 	"fmt"
 	"net/http"
-	"encoding/json" 
+	"encoding/json"
+	"math/rand" 
+	"strings"
 )
 
 
 //json respones : we use struct , then encode it using encoding/json 
 //***for JSON ,, struct key should always start from capital letter or else , it will be 
 //invisible to the json
-type  greetings struct{
-   Message string
+
+
+type URL struct{
+	LongUrl string 
+	ShortUrl string
 }
 
-type Longurl struct{
-	URL string 
+
+
+var urldata = make(map[string]string)
+
+
+//fucn to generate shortcode 
+func generateShortCode()string{
+	var shortcode strings.Builder
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for i:=0;i<=5;i++{
+           shortcode.WriteByte(charset[rand.Intn(len(charset))])
+	}
+	return shortcode.String()
 }
 
-//exatc fucntion handle to be made 
-func printmsg(w http.ResponseWriter , r *http.Request){
-    // write somehtingn to w 
-	fmt.Fprint(w,"hello this is my first go backend code :)")
-	//can also write this using w.Write([]byte(""))
-}
+//create and return shorturl : input : long output : short  + readjson & return json
 
-func readjson(w http.ResponseWriter , r *http.Request){
-	var req Longurl 
-	err:= json.NewDecoder(r.Body).Decode(&req)//similar to sacnl(&s) , we give address of the varibale where we want it to be stored 
+func createShortURL(w http.ResponseWriter , r *http.Request){
+	var url URL 
+	err:=json.NewDecoder(r.Body).Decode(&url)
 	if err!=nil{
-		fmt.Fprintf(w,"error decoding : %v",err)
+		fmt.Fprintf(w,"error in decoding url")
 		return 
 	}
-	fmt.Fprintf(w,"got:%s" , req.URL) 
+	var code string
+	code = generateShortCode()
+	//check is code is already being used or not 
+    _,exists :=urldata[code]
+	if exists{
+		code = generateShortCode()
+	}
+	//update hasmap 
+	urldata[code]=url.LongUrl 
+
+	//return the json with shorturl 
+	shortURL:=fmt.Sprintf("https://localhost:8080/%v",code)
+    
+	json.NewEncoder(w).Encode(shortURL) 
+	
+   
+}
+
+//func to redirect : input:short output :long   + read and return json 
+
+func redirect(w http.ResponseWriter , r *http.Request){
+   var url URL 
+   err:=json.NewDecoder(r.Body).Decode(&url) 
+   if err!=nil{
+		fmt.Fprintf(w,"error in decoding url")
+		return 
+	}
+
 
 }
 
-func prams(w http.ResponseWriter , r * http.Request){
-    name:=r.URL.Query().Get("name")
-	msg := greetings{Message: "hello "+name}  
-	json.NewEncoder(w).Encode(msg)
-}
 
 
 func main(){
-     
-	http.HandleFunc("/",printmsg) //does not return aything 
-	http.HandleFunc("/greet",prams)
-	http.HandleFunc("/shorten",readjson)
+	http.HandleFunc("/createShortURL", createShortURL)
+	http.HandleFunc("/redirectToLongURL", redirect)
 	fmt.Println("server started at localhost 8080 ")
     err := http.ListenAndServe(":8080",nil) //this returns just a error and not usual res,err
 	if(err!=nil){
