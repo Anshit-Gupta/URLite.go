016dd687c33320d309b00b7198ea49b2724f5524
Anshit-Gupta <anshitgupta777@gmail.com>
2026-09-25 19:18:58 +0530

    v1-completed

--- main.go
+++ main.go
@@ -1,110 +1,108 @@
 package main
 
-import(
-	"fmt"
-	"net/http"
+import (
 	"encoding/json"
-	"math/rand" 
-	"strings"
+	"fmt"
+	"math/rand"
+	"net/http"
 	"net/url"
+	"strings"
+	"sync"
 )
 
-
-//json respones : we use struct , then encode it using encoding/json 
-//***for JSON ,, struct key should always start from capital letter or else , it will be 
+//json respones : we use struct , then encode it using encoding/json
+//***for JSON ,, struct key should always start from capital letter or else , it will be
 //invisible to the json
 
-
-type getURL struct{
-	LongUrl string 
-	ShortUrl string
+type ShortenRequest struct {
+	LongURL string
 }
 
-
+//unused
+type ShortenResposne struct {
+	ShortURL string
+}
 
 var urldata = make(map[string]string)
+var mu sync.Mutex
 
-
-//fucn to generate shortcode 
-func generateShortCode()string{
+//fucn to generate shortcode
+func generateShortCode() string {
 	var shortcode strings.Builder
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	for i:=0;i<=5;i++{
-           shortcode.WriteByte(charset[rand.Intn(len(charset))])
+	for i := 0; i <= 5; i++ {
+		shortcode.WriteByte(charset[rand.Intn(len(charset))])
 	}
 	return shortcode.String()
 }
 
 //create and return shorturl : input : long output : short  + readjson & return json
 
-func createShortURL(w http.ResponseWriter , r *http.Request){
-	var Url getURL 
-	err:=json.NewDecoder(r.Body).Decode(&Url)
-	if err!=nil{
-		fmt.Fprintf(w,"error in decoding url")
-		return 
+func createShortURL(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var Url ShortenRequest
+	err := json.NewDecoder(r.Body).Decode(&Url)
+	if err != nil {
+		http.Error(w, "Error decoding the Longurl", 400)
+		return
 	}
 	var code string
 	code = generateShortCode()
-	//check is code is already being used or not 
-    _,exists :=urldata[code]
-	if exists{
+	//check is code is already being used or not
+	//keep generating new code if matching with already present
+	_, exists := urldata[code]
+	for exists {
 		code = generateShortCode()
-	}
-	//update hasmap 
-	urldata[code]=Url.LongUrl 
 
-	//return the json with shorturl 
-	shortURL:=fmt.Sprintf("http://localhost:8080/%v",code)
-    
-	json.NewEncoder(w).Encode(shortURL) 
-	
-   
+		_, exists = urldata[code] //re-check with the new code
+		//here its = and not := , since we are updating the same exist and not creating a new one
+	}
+	//update hasmap
+	urldata[code] = Url.LongURL
+
+	//return the json with shorturl
+	shorturl := fmt.Sprintf("http://localhost:8080/%v", code)
+
+	json.NewEncoder(w).Encode(ShortenResposne{ShortURL: shorturl})
+
 }
 
-//func to redirect : input:short output :long   + read and return json 
+//func to redirect
 
-func redirect(w http.ResponseWriter , r *http.Request){
-	//get the long url 
-   var Url getURL 
-   err:=json.NewDecoder(r.Body).Decode(&Url) 
-   if err!=nil{
-		fmt.Fprintf(w,"error in decoding url")
-		return 
-	}
-	//get the code from the url 
-	u,err:=url.Parse(Url.ShortUrl)
+func redirect(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	//get the code from the short url clicked
+	u, err := url.Parse(r.URL.Path)
 	if err != nil {
-		fmt.Fprintf(w,"error in getting code ")
+		http.Error(w, "Error getting the code ", http.StatusBadRequest)
 		return
 	}
-	code:=strings.TrimPrefix(u.Path,"/")
-    
-	//check if code exist in map 
-	 _,exists :=urldata[code]
-	if !exists{
-		fmt.Fprintf(w,"invalid shorturl ")
+	code := strings.TrimPrefix(u.Path, "/")
+
+	//check if code exist in map
+	_, exists := urldata[code]
+	if !exists {
+		http.Error(w, "Error : code not found", http.StatusNotFound)
 		return
 	}
-    
-	//send map 
-  
-   	json.NewEncoder(w).Encode(urldata[code]) 
+
+	//send map
+
+	http.Redirect(w, r, urldata[code], http.StatusFound)
 
 }
 
-
-
-func main(){
+func main() {
 	http.HandleFunc("/createShortURL", createShortURL)
-	http.HandleFunc("/redirectToLongURL", redirect)
+	http.HandleFunc("/", redirect)
 	fmt.Println("server started at localhost 8080 ")
-    err := http.ListenAndServe(":8080",nil) //this returns just a error and not usual res,err
-	if(err!=nil){
-      fmt.Println("error starting the server at 8080 ",err)
+	err := http.ListenAndServe(":8080", nil) //this returns just a error and not usual res,err
+	if err != nil {
+		fmt.Println("error starting the server at 8080 ", err)
 	}
 
 }
-
-
-
